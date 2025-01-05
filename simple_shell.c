7af0f7895f1c0161d9a6925e6043e948296b389f
Fer <fernando.falcon@holbertonstudents.com>
2025-01-05 16:25:28 +0000

    Create the get_line function to make Betty compliant

--- simple_shell.c
+++ simple_shell.c
@@ -9,31 +9,48 @@
  * Return: 0 or exit status
  */
 
-int main(void)
+char *get_line()
 {
 	char *line = NULL;
 	size_t len = 0;
-	ssize_t n_chars_read;
+	ssize_t line_length;
+
+	line_length = getline(&line, &len, stdin);
+
+	if (line_length == -1)
+	{
+		if (feof(stdin))
+			printf("\n");
+		free(line);
+		return (NULL);
+
+	}
+
+	if (line[line_length - 1] == '\n')
+		line[line_length - 1] = '\0';
+
+	if (strlen(line) == 0)
+	{
+		free(line);
+		return (NULL);
+	}
+
+	return (line);
+}
+
+int main(void)
+{
 	pid_t pid;
 	int status;
+	char *line;
 
 	while (1)
 	{
 		printf("Type something human: ");
 
-		n_chars_read = getline(&line, &len, stdin);
-		if (n_chars_read == -1)
-		{
-			if (feof(stdin))
-				printf("\n");
+		line = get_line();
+		if (line == NULL)
 			break;
-		}
-
-		if (line[n_chars_read - 1] == '\n')
-			line[n_chars_read - 1] = '\0';
-
-		if (strlen(line) == 0)
-			continue;
 
 		pid = fork();
 		if (pid == -1)
@@ -45,12 +62,13 @@ int main(void)
 		if (pid == 0)
 		{
 			char *args[2];
+
 			args[0] = line;
 			args[1] = NULL;
 
-			if (execve(line, args, NULL) == - 1)
+			if (execve(line, args, NULL) == -1)
 			{
-				perror("Execution FAILED");
+				perror("./shell");
 				exit(EXIT_FAILURE);
 			}
 		}
@@ -61,5 +79,5 @@ int main(void)
 	}
 
 	free(line);
-	return 0;
+	return (0);
 }
