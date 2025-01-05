bd05310febd5b24fda38496feabbc13739626c7a
Fer <fernando.falcon@holbertonstudents.com>
2025-01-05 17:30:03 +0000

    Header added

--- simple_shell.c
+++ simple_shell.c
@@ -1,8 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
+#include "simple_shell.h"
 
 /**
  * main - UNIX (only) command line interpreter
---
+++ simple_shell.h
@@ -0,0 +1,10 @@
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#endif
